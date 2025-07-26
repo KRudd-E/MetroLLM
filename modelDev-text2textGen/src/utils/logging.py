73a1c51af6f8b1c0dafb1127ef0d8e2422b13f85
KRooL4E <kieranrudd0815@gmail.com>
2025-07-26 20:24:34 +0100

    t2t logging.py updated

--- modelDev-text2textGen/src/utils/logging.py
+++ modelDev-text2textGen/src/utils/logging.py
@@ -1,4 +1,4 @@
-from transformers import TrainerCallback
+from transformers.trainer_callback import TrainerCallback
 import os
 import json
 import datetime
@@ -26,7 +26,7 @@ class LoggingCallback(TrainerCallback):
 
 
 class DebugCallback(TrainerCallback):
-    def on_step_end(self, state, logs=None):
+    def on_step_end(self, args, state, control, logs=None, metrics=None, **kwargs):
         if logs and "loss" in logs:
             if logs["loss"] == 0:
                 print("WARNING: Zero loss detected!")

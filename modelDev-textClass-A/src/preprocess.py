0d06d107fd906cd72b9d20c60e0d452a22844bed
KRooL4E <kieranrudd0815@gmail.com>
2025-08-19 20:20:12 +0100

    updated config file paths.

--- modelDev-textClass-A/src/preprocess.py
+++ modelDev-textClass-A/src/preprocess.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MultiLabelBinarizer
+
 from datasets import Dataset
 from transformers.models.auto.tokenization_auto import AutoTokenizer
 

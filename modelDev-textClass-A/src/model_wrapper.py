41b27d00be5f56cdd9a0eb0b7c5cd35bf588e817
KRooL4E <kieranrudd0815@gmail.com>
2025-08-20 09:40:10 +0100

    tc-a model_wrapper

--- modelDev-textClass-A/src/model_wrapper.py
+++ modelDev-textClass-A/src/model_wrapper.py
@@ -18,25 +18,28 @@ class ClassificationWrapper:
             model_config = AutoConfig.from_pretrained(
                 config["model"]["name"],
                 reference_compile=False,
+                num_labels=config['data']['class_no'],
+                problem_type="multi_label_classification"
             )
             
             self.model = AutoModelForSequenceClassification.from_pretrained(
                 config["model"]["name"],
-                config=model_config,
-                num_labels=config['data']['class_no'],
-                problem_type="multi_label_classification"
+                config=model_config
             )
             self.tokenizer = AutoTokenizer.from_pretrained(
                 config["model"]["name"], use_fast=True
             )
         else:
             
-            #! MAY NEED MODEL_CONFIG 
+            model_config = AutoConfig.from_pretrained(
+                config["model"]["source_dir"],
+                num_labels=config['data']['class_no'],
+                problem_type="multi_label_classification"
+            )
             
             self.model = AutoModelForSequenceClassification.from_pretrained(
                 config["model"]["source_dir"],
-                num_labels=config['data']['class_no'],
-                problem_type="multi_label_classification",
+                config=model_config,
                 low_cpu_mem_usage=True,
                 device_map="auto",
             )

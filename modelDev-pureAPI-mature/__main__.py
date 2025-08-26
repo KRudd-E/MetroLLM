8e18af77f7420aabbf2ea8b7a2d60f9f970fb47e
KRooL4E <kieranrudd0815@gmail.com>
2025-08-26 17:20:27 +0100

    pureAPI -> pureAPI-mature

---
+++ modelDev-pureAPI-mature/__main__.py
@@ -0,0 +1,8 @@
+from src.pipeline import Pipeline
+
+if __name__ == "__main__":
+    pipeline = Pipeline()
+    pipeline.run()
+    
+# NB: config_path = 'modelDev/config.yaml' 
+# To alter, see modelDev/src/misc/utils.py

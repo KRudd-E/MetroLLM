2fde928f4acf16c4ae86aec464529098c357df55
KRooL4E <kieranrudd0815@gmail.com>
2025-09-06 23:07:11 +0100

    Var. additions

--- modelDev-textClass-C/__main__.py
+++
@@ -1,8 +0,0 @@
-from src.pipeline import FineTunePipeline
-
-if __name__ == "__main__":
-    pipeline = FineTunePipeline()
-    pipeline.run()
-    
-# NB: config_path = 'modelDev/config.yaml' 
-# To alter, see modelDev/src/misc/utils.py
